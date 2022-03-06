04fe7b8a4a120a0b537442c5bf5bbff61f8c32bb
federicoestebanarnau <federicoestebanarnau@gmail.com>
2022-03-06 02:54:41 -0300

    Updates

---
+++ precache-manifest.924d038850cc3198f339e106749b1a9f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d17404db4816d500292",
+    "url": "/fedearnau/static/css/main.eef620e2.chunk.css"
+  },
+  {
+    "revision": "0d17404db4816d500292",
+    "url": "/fedearnau/static/js/main.0d17404d.chunk.js"
+  },
+  {
+    "revision": "b57061c7c54bbdd02d04",
+    "url": "/fedearnau/static/js/1.b57061c7.chunk.js"
+  },
+  {
+    "revision": "6a7c930cf7a3d99297a2",
+    "url": "/fedearnau/static/css/2.0e3f4c05.chunk.css"
+  },
+  {
+    "revision": "6a7c930cf7a3d99297a2",
+    "url": "/fedearnau/static/js/2.6a7c930c.chunk.js"
+  },
+  {
+    "revision": "056ac31d81ab2d926f3f",
+    "url": "/fedearnau/static/js/runtime~main.056ac31d.js"
+  },
+  {
+    "revision": "e24fb87ebb3c375bb364c80d003624dc",
+    "url": "/fedearnau/static/media/CV_Federico_Esteban_Arnau.e24fb87e.pdf"
+  },
+  {
+    "revision": "d9ab38c856212e1bf2b418b44689b034",
+    "url": "/fedearnau/static/media/home-main.d9ab38c8.svg"
+  },
+  {
+    "revision": "bc2c7afa280d7a7d54e2c5ecf515e8a6",
+    "url": "/fedearnau/static/media/home-bg.dc52d5d4.jpg"
+  },
+  {
+    "revision": "70d9e7942b7d9192c7d8a20a39030eff",
+    "url": "/fedearnau/static/media/pre.70d9e794.svg"
+  },
+  {
+    "revision": "ca28f2e86d1f1359f309078a49b5931e",
+    "url": "/fedearnau/static/media/yo.9187dd24.jpeg"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/fedearnau/static/media/about.aee0f771.png"
+  },
+  {
+    "revision": "a7c83597c3c04e0416a57eff4845faa1",
+    "url": "/fedearnau/static/media/cv_image.5f479ac1.jpg"
+  },
+  {
+    "revision": "2798a479741d70a8ac9a06cb13002496",
+    "url": "/fedearnau/index.html"
+  }
+];
