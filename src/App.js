84de967cdd5918c2bfd8c1700600eb23d393b1c9
federicoestebanarnau <federicoestebanarnau@gmail.com>
2022-03-08 12:14:06 -0300

    aa

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         <Navbar />
         
         <Switch>
-          <Route path="/" component={Home}  /> 
+          <Route  exact path="/" component={Home}  /> 
           <Route path="/Project" component={Projects} />
           <Route path="/About" component={About} />
           <Route path="/Resume" component={Resume} />
