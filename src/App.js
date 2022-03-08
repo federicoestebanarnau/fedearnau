707cc9fccd10979bf81910ab8888770ec97003dc
federicoestebanarnau <federicoestebanarnau@gmail.com>
2022-03-08 10:20:32 -0300

    new

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         <Navbar />
         
         <Switch>
-          <Route exact path="https://federicoestebanarnau.github.io/fedearnau/" component={Home} />
+          <Route  path="/" component={Home} />
            <Route path="/Project" component={Projects} />
            <Route path="/About" component={About} />
            <Route path="/Resume" component={Resume} /> 
